26d8da702d5d0b93a1ff9703521c4e6ead8a631d
barsre <eaajtb@students.eaaa.dk>
2018-05-21 10:22:58 +0200

    Header
    En include med Headeren i er inkluderet.

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<?php
+echo '<header>
+        <img src="img/Logo.jpg">
+        <ul>
+            <a href="Index.php"><li>Frontpage</li></a>
+            <a href="Index.php"><li>Products</li></a>
+            <a href="About.php"><li>About</li></a>
+            <a href="Contact.php"><li>Contact</li></a>
+        </ul>
+    </header>';
+?>
