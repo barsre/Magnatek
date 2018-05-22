ad4e0c196a1cd1a876fbb7276662e92a99c247ea
barsre <eaajtb@students.eaaa.dk>
2018-05-22 08:58:02 +0200

    Startet på frontpage
    Begyndt at lave Frontpage mandag d. 21/5

--- Index.php
+++ Index.php
@@ -15,7 +15,33 @@
 <body>
 
     <?php include 'includes/header.php';?>
-
+    
+    <section class="video">
+    </section>
+    
+    <section class="product">
+        <button class="product__button">SEE PRODUCTS</button>
+    </section>
+    
+    <section class="heighlights">
+        <img src="img/White_logo.gif">
+        <ul>
+            <li><b>25</b><div></div>years of experience in X-ray sources.</li>
+            <li><b>3000</b><div></div>X-ray sources we have supplied the industry with.</li>
+            <li><b>12</b><div></div>experienced employees with solid engineering background.</li>
+            <li><b>40%</b><div></div>of our sales are directed to the European market.</li>
+        </ul>
+    </section>
+    
+    <section class="info">
+        <img src="img/pic1.jpeg">
+        <p>Magnatek is a leading and innovative company providing highly stabilised, high frequency X-ray sources and high voltage X-ray generators for medical and industrial instrumentation.<br>
+        The controllers and generators are designed and manufactured by Magnatek with voltages up to 250kV and more than 1kW power.<br>
+        Our products are installed and used in a vast array of applications in industrial, medical and scientific communities.<br>
+        To Magnatek your success means everything.<br>
+        Feel free to contact us and let’s find the optimal solution to your needs.</p>
+    </section>
+    
 </body>
 
 <script src="js/script.js"></script>
