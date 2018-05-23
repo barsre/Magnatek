a3b4eff0f2bdc255028e26782dd3f774f9bfe7dc
barsre <eaajtb@students.eaaa.dk>
2018-05-23 12:05:55 +0200

    Opdelling ad Stylesheets + Ny Frontpage

--- Index.php
+++ Index.php
@@ -6,7 +6,8 @@
 	<meta charset="utf-8">
 	<title>Dom tree example</title>
 	
-	<link href="css/stylesheet.css" rel="stylesheet">
+    <link href="css/stylesheet_global.css" rel="stylesheet">
+	<link href="css/stylesheet_frontpage.css" rel="stylesheet">
 	
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     
@@ -16,29 +17,29 @@
 
     <?php include 'includes/header.php';?>
     
-    <section class="video">
+    <section id="frontpage" class="frontpage">
+        <h1 class="frontpage__header">We modify our products to match your<br>needs the best way possible</h1>
+        <div class="frontpage__content">
+            <h1><b>25</b><br><div></div>years of experience in X-ray sources.</h1>
+            <img src="img/frontpage/pic1.gif">
+            <h1><b>3000</b><br><div></div>X-ray sources we have supplied the industry with.</h1>
+        </div>
     </section>
     
-    <section class="product">
-        <button class="product__button">SEE PRODUCTS</button>
+    <section id="product" class="product nodisplay">
+        <h1>Application Areas</h1>
+        <div class="product__categories">
+            <button id="category_1">Industrial measuring and analysis</button>
+            <button id="category_2">Medical Research</button>
+            <button id="category_3">Non-destructive testing and measuring</button>
+            <button id="category_4">Electronics circuit board inspection</button>
+            <button id="category_5">Security screening</button>
+            <button id="category_6">Food inspection & sorting</button>
+        </div>
     </section>
     
-    <section class="heighlights">
-        <img src="img/White_logo.gif">
-        <ul>
-            <li><b>25</b><div></div>years of experience in X-ray sources.</li>
-            <li><b>3000</b><div></div>X-ray sources we have supplied the industry with.</li>
-            <li><b>12</b><div></div>experienced employees with solid engineering background.</li>
-            <li><b>40%</b><div></div>of our sales are directed to the European market.</li>
-        </ul>
-    </section>
-    
-    <section class="info">
-        <p>Magnatek is a leading and innovative company providing highly stabilised, high frequency X-ray sources and high voltage X-ray generators for medical and industrial instrumentation.<br>
-        The controllers and generators are designed and manufactured by Magnatek with voltages up to 250kV and more than 1kW power.<br>
-        Our products are installed and used in a vast array of applications in industrial, medical and scientific communities.<br>
-        To Magnatek your success means everything.<br>
-        Feel free to contact us and let’s find the optimal solution to your needs.</p>
+    <section class="product_button">
+        <button id="product_button" onclick="Open_product()">SEE PRODUCTS</button>
     </section>
     
     <?php include 'includes/footer.php';?>
@@ -48,9 +49,13 @@
 <script src="js/script.js"></script>
 
 <script>
-
+    var Underline = document.createElement("DIV");
+    var Linebreak = document.createElement("BR");
+    document.getElementById("menubutton_1").appendChild(Linebreak);
+    document.getElementById("menubutton_1").appendChild(Underline);
     
-    
+    var Onpage = document.getElementById("menubutton_1");
+    Onpage.classList.add("onpage");
 </script>    
     
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+var Frontpage = document.getElementById("frontpage");
+var Productpage = document.getElementById("product");
+var ProductButton = document.getElementById("product_button");
+
+function Open_product() {
+    Frontpage.classList.add("nodisplay");
+    Productpage.classList.remove("nodisplay");
+    ProductButton.onclick = Close_product;
+    ProductButton.innerHTML = "CLOSE PRODUCTS";
+}
+
+function Close_product() {
+    Frontpage.classList.remove("nodisplay");
+    Productpage.classList.add("nodisplay");
+    ProductButton.onclick = Open_product;
+    ProductButton.innerHTML = "SEE PRODUCTS";
+}
