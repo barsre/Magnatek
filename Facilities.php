be2c80ae764733a6473d33141320a26f25e0f548
Jeevith <jeevithm@yahoo.dk>
2018-05-25 09:26:49 +0200

    opdateringer

--- Facilities.php
+++ Facilities.php
@@ -22,12 +22,17 @@
 <div class="facilities">
 	<section class="facilities__info">
 		<p class="facilities__info__text">Magnatek has expanded our facilities from 375m² to more than 1000m² and it includes space for development, production, storage, test and administration.
-At our facilities, we can test the performance of new X-ray sources together with different kinds of X-ray sensitive detectors and PC controlled scanning devices.The first feasibility studies of several of our customers successful projects, have been started in our laboratory.
+		</p>
+			<p class="facilities__info__text">
+At our facilities, we can test the performance of new X-ray sources together with different kinds of X-ray sensitive detectors and PC controlled scanning devices. The first feasibility studies of several of our customers successful projects, have been started in our laboratory. 	In 1991 Kim Demandt, who held a significant experience and expertise in industrial X-ray, bought half of the company and the company’s focus changed to X-ray equipment. The company moved to Thoravej in Copenhagen in 2002, where the facilities covered 300 m².
+
 </p>
+
+	</section>
+	<section>
 	<img class="facilities__info__img" src="img/facilities/Billede1.jpg" alt="Facilities picture">
 	<img class="facilities__info__hus" src="img/facilities/hus.jpg" alt="facilities picture">
-	</section>
-
+		</section>
 </div>
 
 
