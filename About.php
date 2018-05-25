6d7ba1a575f496a0d516114ea7682923b0203a8a
kn4291 <36158366+kn4291@users.noreply.github.com>
2018-05-25 11:06:16 +0200

    startet på aboutpage

--- About.php
+++ About.php
@@ -6,7 +6,8 @@
 	<meta charset="utf-8">
 	<title>Dom tree example</title>
 	
-	<link href="css/stylesheet_global.css" rel="stylesheet">
+	<link href="css/stylesheet.css" rel="stylesheet">
+	<link href="css/about.stylesheet.css" rel="stylesheet">
 	
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     
@@ -15,19 +16,25 @@
 <body>
 
     <?php include 'includes/header.php';?>
+    
+      <section class="info">
+
+        <p>Magnatek is a leading and innovative company providing highly stabilised, high frequency X-ray sources and high voltage X-ray generators for medical and industrial instrumentation.<br>
+        The controllers and generators are designed and manufactured by Magnatek with voltages up to 250kV and more than 1kW power.<br>
+        Our products are installed and used in a vast array of applications in industrial, medical and scientific communities.<br>
+        To Magnatek your success means everything.<br>
+        Feel free to contact us and let’s find the optimal solution to your needs.</p>
+    </section>
+    
+    <div class="pic">
+        <img src="img/about/magnatek.henrik.jpeg" alt="billede af Henrik fra Magnatek"> 
+    </div>
+
+
+    <?php include 'includes/footer.php';?>
 
 </body>
 
 <script src="js/script.js"></script>
 
-<script>
-    var Underline = document.createElement("DIV");
-    var Linebreak = document.createElement("BR");
-    document.getElementById("menubutton_3").appendChild(Linebreak);
-    document.getElementById("menubutton_3").appendChild(Underline);
-    
-    var Onpage = document.getElementById("menubutton_3");
-    Onpage.classList.add("onpage");
-</script>
-    
 </html>
