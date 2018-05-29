4cc07c3e63190ed481e384903f4030a46c059d73
barsre <eaajtb@students.eaaa.dk>
2018-05-29 11:31:33 +0200

    Fixet Hearder fejl

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,9 @@
-<?php
-echo '<header>
-        <a href="Index.php"><img src="img/general/Logo.jpg"></a>
-        <ul>
-            <a href="Index.php" id="menubutton_1"><li>Frontpage</li><br><div></div></a>
-            <a href="Index.php" id="menubutton_2"><li>Products</li><br><div></div></a>
-            <a href="About.php" id="menubutton_3"><li>About</li><br><div></div></a>
-            <a href="Contact.php" id="menubutton_4"><li>Contact</li><br><div></div></a>
-        </ul>
-    </header>';
-?>
+<header>
+    <a href="Index.php"><img src="img/general/Logo.jpg"></a>
+    <ul>
+        <a href="Index.php" id="menubutton_1"><li>Frontpage</li><br><div></div></a>
+        <a href="Index.php#products" id="menubutton_2"><li>Products</li><br><div></div></a>
+        <a href="About.php" id="menubutton_3"><li>About</li><br><div></div></a>
+        <a href="Contact.php" id="menubutton_4"><li>Contact</li><br><div></div></a>
+    </ul>
+</header>
